ec1fb187135f941e160593a548d93346cdde8876
aloshvp <aloshvp12@gmail.com>
2025-08-16 23:54:49 +0530

    day1

--- day1.js
+++ day1.js
@@ -57,5 +57,62 @@
 // console.log({} + {});//[object object] [object object]
 
 
-console.log(1 < 2 < 3);
-console.log(3 > 2 > 1);
+// console.log(1 < 2 < 3);//true
+// console.log(3 > 2 > 1);//false
+
+// console.log("10" - "4");//6
+// console.log("10" + "4");//"104"
+// console.log("10" * "4");//40
+// console.log("10" / "2");//5
+
+
+// console.log(typeof null);//object
+// console.log(null instanceof Object);//false
+
+// let a = [1, 2, 3];
+// let b = [1, 2, 3];
+// console.log(a == b);//false
+// console.log(a === b);//false
+
+
+// console.log([1, 2] + [3, 4]);//1,23,4
+
+// console.log(!!"");//false
+// console.log(!!"hello");//true
+// console.log(!!0);//false
+// console.log(!!1);//true
+
+// console.log(typeof NaN);//number
+// console.log(isNaN("hello"));//true
+// console.log(Number.isNaN("hello"));//false
+
+// console.log("5" - - "2");//7
+// console.log("5" + + "2"); //52
+
+// console.log([1, 2, 3] == "1,2,3"); //true
+
+// console.log(0 == false);//true
+// console.log(0 === false);//false
+
+// console.log(undefined == null);//true
+// console.log(undefined === null);//false
+
+
+// console.log(NaN == NaN);//false
+// console.log(Object.is(NaN, NaN));//true
+
+// console.log(+true);//1
+// console.log(+false);//0
+
+// console.log([] == 0);//true
+// console.log([] == ![]);//true
+
+
+
+
+
+
+
+
+
+
