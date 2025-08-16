841c23d110cb989a0da4c40415bfa18490665999
aloshvp <aloshvp12@gmail.com>
2025-08-16 23:39:48 +0530

    day1

---
+++ script.js
@@ -0,0 +1,61 @@
+
+
+// function reverse(str) {
+//     return str.split("").reverse().join('')
+// }
+// console.log(reverse("alosh"));
+
+
+// function palindrome(str) {
+//     const reversed = str.split("").reverse().join("");
+//     return str === reversed
+// }
+// console.log(palindrome("amma"));
+
+// function duplicate(arr) {
+//     return [...new Set(arr)];
+// }
+// console.log(duplicate([1, 1, 2, 3, 4, 4]))
+
+// Flatten a Nested Array
+// function flattenArray(arr) {
+//     return arr.flat(Infinity);
+// }
+// console.log(flattenArray([1, [2, 3, [4, 5]]])); // [1,2,3,4,5]
+
+// Remove Duplicates from Array
+// function removeDuplicates(arr) {
+//     return [...new Set(arr)];
+// }
+// console.log(removeDuplicates([1, 2, 2, 3, 3, 4])); // [1,2,3,4]
+
+
+// function maxMin(arr) {
+// return { max: Math.max(...arr), min: Math.min(...arr) };
+// console.log(Math.max(...arr));
+// console.log(Math.min(...arr));
+// }
+// console.log(maxMin([5, 3, 9, 1])); // { max: 9, min: 1 }
+
+
+// function flattenArray(arr) {
+//     return arr.flat(Infinity);
+// }
+// console.log(flattenArray([1, [2, 3, [4, 5]]])); // [1,2,3,4,5]
+
+
+// Merge Two Arrays & Remove Duplicates
+// function mergeArrays(a, b) {
+//     return [...new Set([...a, ...b])];
+// }
+// console.log(mergeArrays([1, 2, 3], [2, 3, 4])); // [1,2,3,4]
+
+
+// console.log([] + []);//""
+// console.log([] + {});//[object object]
+// console.log({} + []);//[object object]
+// console.log({} + {});//[object object] [object object]
+
+
+console.log(1 < 2 < 3);
+console.log(3 > 2 > 1);
