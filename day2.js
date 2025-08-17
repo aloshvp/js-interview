93fa04a21b6965a3dc240c4472f4a61441a4c0a8
aloshvp <aloshvp12@gmail.com>
2025-08-17 23:42:44 +0530

    day2 hour1

--- day2.js
+++ day2.js
@@ -1,12 +1,12 @@
 // Function Declaration
-function add(a, b) {
-    return a + b;
-}
+// function add(a, b) {
+//     return a + b;
+// }
 
 // Function Expression
-const multiply = function (a, b) {
-    return a * b;
-};
+// const multiply = function (a, b) {
+//     return a * b;
+// };
 
 // Declaration: hoisted completely → can call before definition.
 // Expression: not hoisted → can only call after assignment.
@@ -16,6 +16,28 @@ const multiply = function (a, b) {
 // ---------------------------------------------------------------------------------
 
 
+// Arrow functions vs normal functions
+
+// const normalFunc = function () { console.log(this); };
+// const arrowFunc = () => { console.log(this); };
+
+// Arrow functions do not have their own this → inherit from enclosing lexical scope.
+// Cannot be used as constructors.
+// No arguments object(use rest parameters instead).
+
+// ---------------------------------------------------------------------------------
 
 
 
+
+
+
+
+
+
+
+// ---------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------
