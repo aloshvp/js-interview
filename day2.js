5ac0e0ba193d0473a8206b7b0aeabb5bea67278e
aloshvp <aloshvp12@gmail.com>
2025-08-17 23:17:44 +0530

    days hour1

---
+++ day2.js
@@ -0,0 +1,21 @@
+// Function Declaration
+function add(a, b) {
+    return a + b;
+}
+
+// Function Expression
+const multiply = function (a, b) {
+    return a * b;
+};
+
+// Declaration: hoisted completely → can call before definition.
+// Expression: not hoisted → can only call after assignment.
+// Function expressions can be anonymous or named.
+
+
+// ---------------------------------------------------------------------------------
+
+
+
+
+
